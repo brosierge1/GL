ac4f2ed9e33b402bd93187ca19e7d7e1b828b5eb
brosier <bruce.rosier@u-psud.fr>
2018-11-13 22:27:06 +0100

    exo terminé

--- src/main.c
+++ src/main.c
@@ -2,8 +2,55 @@
 //
 
 #include "my_header.h"
+#include <stdlib.h> 
 
 int main(void)
 {
+  unsigned char n=1;
+  unsigned short int s1=3;
+  unsigned short int s2=3
+
+//question 1//
+  while(n<12)
+  {
+   s1=s1+3*n;
+   n=n+1
+
+  }
+//question 2//
+  while(s2<9876)
+   {
+    s2=s2+3*n;
+    n=n+1;
+   }
+//question 3//
+//VSCode ne trouve pas stdlib.h
+
+
+  }
+      /* data */
+  ;
+    
     return 0;
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
