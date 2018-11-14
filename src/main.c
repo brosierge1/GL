2dc79e2c4aad9dd65c70412d77830920c1e729c6
brosier <bruce.rosier@u-psud.fr>
2018-11-14 21:55:20 +0100

    exo 43 terminé

--- src/main.c
+++ src/main.c
@@ -2,21 +2,30 @@
 //
 
 #include "my_header.h"
-#include <stdlib.h> 
+#include <stdlib.h>
+
 
 int main(void)
 {
-  unsigned char n=1;
+  unsigned char n=2;
   unsigned short int s1=3;
-  unsigned short int s2=3
+  unsigned short int s2=3;
+   int s3=0;
+  int a=((rand())%10)+1;
+  unsigned short int s4=0;
+  char i=0;
+  char h=0;
+  unsigned short int c=0;
+  
 
 //question 1//
   while(n<12)
   {
    s1=s1+3*n;
-   n=n+1
+   n=n+1;
 
   }
+	n=2;
 //question 2//
   while(s2<9876)
    {
@@ -24,14 +33,31 @@ int main(void)
     n=n+1;
    }
 //question 3//
-//VSCode ne trouve pas la biblio stdlib.h//
-
-
+  while(i<12)
+  {
+	s3=s3+a;
+    i=i+1;
+	a=((rand())%10)+1;
+  }
+//question 4//
+   while(h<20){
+   if(c%3==0 || c%7==0){
+	 s4=s4+c;
+	 c=c+1;
+	 h=h+1;
+   }
+   
+   else
+	   c=c+1;
+   
+   
+   }
+	return 0;
   }
       /* data */
   ;
     
-    return 0;
+   
 
   
 
@@ -41,16 +67,4 @@ int main(void)
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-}
 
