13ae158d43e6e254b0f1b66b1c634e270373c449
bruce rosier <bruce.rosier@ge1.iut-cachan.u-psud.fr>
2018-11-20 09:47:17 +0100

    exo commencé

--- src/main.c
+++ src/main.c
@@ -1,10 +1,57 @@
-//  Copyright (c) 2018 Antoine Tran Tan
-//
-
-#include "my_header.h"
+#define  SEUIL 5
+#define SEUILH 200
+#define SEUILB -100
+#include <ansi_c.h>
+#include "TP_lib.h"
 
 int main(void)
 {
-    return 0;
+	unsigned char bin [10]={1,2,3,4,5,6,7,8,9,10};
+	unsigned char i;
+	unsigned char sat [10]=((rand())%10);
+	
+	
+	for(i=0; i<10;i=i+1)
+	{
+		
+	if(i<SEUIL)
+		bin[i]=0;
+	else if (i>=SEUIL)
+		bin[i]=1;
+	
+		
+	}
+
+	
+	
+	for(i=0; i<10;i=i+1)
+	{
+	   if(bin[i]>SEUILH)
+		   bin[i];
+	
+	
+	
+	
+	
+	
+	
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+   return 0;
+   
 }
- 
