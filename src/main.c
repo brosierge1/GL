7ef2c0afec75f5d88a8d8587e933430c754e1908
bruce rosier <bruce.rosier@ge1.iut-cachan.u-psud.fr>
2018-11-27 09:46:41 +0100

    exo58

--- src/main.c
+++ src/main.c
@@ -1,9 +1,56 @@
-//  Copyright (c) 2018 Antoine Tran Tan
-//
+	 
 
-#include "my_header.h"
 
-int main(void)
-{
-    return 0;
-}
+#include  "Tp_lib_suite.h"
+	initialiserTirage()
+	{
+	 unsigned char gagnant[6]={tirerNumero(),tirerNumero(),tirerNumero(),tirerNumero(),tirerNumero(),tirerNumero()};   
+	}
+
+
+
+
+	int main(void)
+	{
+	unsigned char joueur[6]={1,12,32,41,25,4};
+	unsigned char i;
+	unsigned char nbgagnants=0;
+	
+	 
+	initialiserTirage();
+	for(i=0;i<6;i++)
+	{
+		
+	 if( joueur[0]==gagnant[i])
+		nbgagnants=nbgagnants+1;
+	 
+	if (joueur[1]==gagnant[i])
+		nbgagnants=nbgagnants+1;
+		
+	if (joueur[2]==gagnant[i])
+		nbgagnants=nbgagnants+1;
+		
+	if (joueur[3]==gagnant[i])
+		nbgagnants=nbgagnants+1;
+		
+	if (joueur[4]==gagnant[i])
+		nbgagnants=nbgagnants+1;
+		
+	if (joueur[5]==gagnant[i])
+		nbgagnants=nbgagnants+1;
+		
+		
+	 
+	 
+	
+	
+	
+	
+	}
+	
+	
+	
+	return 0;
+	
+	
+	} 
