57cb17f0643b6825a30f800981d4553a5b7ebd8c
bruce rosier <bruce.rosier@ge1.iut-cachan.u-psud.fr>
2018-12-04 09:42:56 +0100

    exo 79 terminé

--- src/main.c
+++ src/main.c
@@ -1,9 +1,31 @@
-//  Copyright (c) 2018 Antoine Tran Tan
-//
+#include <userint.h>
+#include <ansi_c.h>
 
-#include "my_header.h"
+
 
 int main(void)
 {
-    return 0;
+char phrase[100];
+
+static char libelle[81];
+char prenom[100];
+char nom[100];
+long unsigned int age;
+long unsigned int an;
+
+
+
+scanf("%s %s %lu",prenom,nom,&an);
+
+
+age=2018-an;
+
+
+printf("je m'appelle %s %s et j'ai %d ans",prenom,nom,age);
+
+sprintf(libelle,"je m'appelle %s %s et j'ai %lu ans",prenom,nom,age);
+MessagePopup("boum",libelle);
+
+return 0;
+
 }
